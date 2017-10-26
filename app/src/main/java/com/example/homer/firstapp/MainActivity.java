067b90d83f388f765d2eecfa15b032a1de7a5068
ami95 <mihaiadrian1995@gmail.com>
2017-10-26 14:28:51 +0300

    #9 passing data into string

--- app/src/main/java/com/example/homer/firstapp/MainActivity.java
+++ app/src/main/java/com/example/homer/firstapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSomething();
+
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        seekBar.setMax(100);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         bt = (Button) findViewById(R.id.button);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,4 +51,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void getSomething () {
+        Toast.makeText(this, "getSomething method called", Toast.LENGTH_SHORT).show();
+    }
+
 }
